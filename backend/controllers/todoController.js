be03ae3c1138488c74b80ea1c604fbe3340bd3e5
Mohab Khaled <mohabk.2006@gmail.com>
2026-09-14 18:51:46 +0300

    Backend task completed

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,42 +1,49 @@
-const Todo = require('../models/Todo');
+const Todo = require("../models/Todo");
 
 const getTodos = async (req, res) => {
   try {
-    const todos = await Todo.find().sort({ createdAt: -1 });
-    res.json(todos);
+    let filter = {};
+    if (req.query.done === undefined) filter = {};
+    else filter.done = req.query.done === "true";
+
+    const todos = await Todo.find(filter).sort({ createdAt: -1 });
+    res.status(200).json(todos);
   } catch (err) {
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: "Server error" });
   }
 };
 
 const createTodo = async (req, res) => {
-  if (!req.body.title) return res.status(400).json({ error: 'Title is required' });
-  
+  if (!req.body.title)
+    return res.status(400).json({ error: "Title is required" });
+
   try {
     const newTodo = await Todo.create({ title: req.body.title });
     res.status(201).json(newTodo);
   } catch (err) {
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: "Server error" });
   }
 };
 
 const updateTodo = async (req, res) => {
   try {
-    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!updatedTodo) return res.status(404).json({ error: 'Todo not found' });
+    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!updatedTodo) return res.status(404).json({ error: "Todo not found" });
     res.json(updatedTodo);
   } catch (err) {
-    res.status(400).json({ error: 'Invalid ID' });
+    res.status(400).json({ error: "Invalid ID" });
   }
 };
 
 const deleteTodo = async (req, res) => {
   try {
     const deletedTodo = await Todo.findByIdAndDelete(req.params.id);
-    if (!deletedTodo) return res.status(404).json({ error: 'Todo not found' });
-    res.json({ message: 'Todo deleted' });
+    if (!deletedTodo) return res.status(404).json({ error: "Todo not found" });
+    res.json({ message: "Todo deleted" });
   } catch (err) {
-    res.status(400).json({ error: 'Invalid ID' });
+    res.status(400).json({ error: "Invalid ID" });
   }
 };
 
@@ -44,5 +51,5 @@ module.exports = {
   getTodos,
   createTodo,
   updateTodo,
-  deleteTodo
+  deleteTodo,
 };
